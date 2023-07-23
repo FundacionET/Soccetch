94cd12aab7ebd397aec49fb843e64dc06c9fe9fa
Sergio Galvis <chefosergio@gmail.com>
2023-07-23 16:25:18 -0500

    header del dashboard en proceso

--- frontend/src/components/Dashboard/Header/index.header.jsx
+++ frontend/src/components/Dashboard/Header/index.header.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { ContHeader, Titulo } from './styled.header';
+import { ContHeader,ContainerPerfilRol, ContainerProfileNameRol, ContainerSetting, Menu, Name, Perfil, Rol, Titulo } from './styled.header';
 import { LogoSocetech } from '../../Header/StyleHeader';
 import * as imagenes from '../../img'
 
@@ -8,8 +8,19 @@ const HeaderP = () => {
   return ( 
     <Fragment>
       <ContHeader>
+        <Menu>
+          
+        </Menu>
         <LogoSocetech src={imagenes.HeaderLogo} alt='Logo'/>
         <Titulo>SocceTech</Titulo>
+        <ContainerProfileNameRol>
+          <Perfil></Perfil>
+          <ContainerPerfilRol>
+            <Name>Sergio Galvis</Name>
+            <Rol>Administrador</Rol>
+          </ContainerPerfilRol>
+        </ContainerProfileNameRol>
+        <ContainerSetting></ContainerSetting>
       </ContHeader>
     </Fragment>
   );
--- frontend/src/components/Dashboard/Header/styled.header.jsx
+++ frontend/src/components/Dashboard/Header/styled.header.jsx
@@ -1,23 +1,77 @@
 import styled from 'styled-components';
 
 export const ContHeader = styled.header`
-  height: 6.25rem;
+  height: 5rem;
   width: 100%;
   background-color: #000;
   display: flex;
   align-items: center;
+  justify-content: space-evenly;
   
 `;
 
 export const Menu = styled.button`
+  border-radius: 100%;
+  height: 2rem;
+  width: 2rem;
+  background-color: #0e0e0e;
+  outline: none;
+  border: none;
+  display: flex;
+  align-items: center;
+`
+export const MenuLogo = styled.img`
   
 `
 
-export const Titulo = styled.div`
+export const Titulo = styled.button`
   font-family: 'Faster One', cursive;
   color: white;
   font-size: 4rem;
+  background-color: #000000;
+  outline: none;
+  border: none;
+  cursor: pointer;
+`
+export const ContainerProfileNameRol = styled.div`
+  border: 1px solid white;
+  width: 12rem;
+  height: 3.125rem;
+  display: flex;
+  justify-content: space-around;
+  
+
 `
 export const Perfil = styled.div`
   border: 1px solid red;
+  border-radius: 100%;
+  width: 3rem;
+  height: 3rem;
+`
+export const ContainerPerfilRol = styled.div`
+  height: 1.5rem;
+  border: 1px solid blue;
+  
+`
+export const Name = styled.div`
+  border: 1px solid red;
+  height: auto;
+  width: auto;
+  color: white;
+  
+  
+`
+export const Rol = styled.div`
+  border: 1px solid red;
+  height: auto;
+  width: auto;
+  color: white;
+`
+export const ContainerSetting = styled.button`
+  border: none;
+  outline: none;
+  border: 1px solid aqua;
+  width: 3rem;
+  height: 3rem;
+  background-color: black;
 `
